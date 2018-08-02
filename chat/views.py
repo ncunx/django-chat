5915932eaba99f59c8ab3b62fcba65170957fc26
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-08-02 19:03:18 +0100

    Set up basics of channels

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+
+def room(request, room):
+    return render(request, 'chat/room.html', {
+        'room_json': mark_safe(json.dumps(room))
+    })
